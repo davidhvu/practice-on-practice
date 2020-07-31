1d1ed631d86aa4c0d1241aee102abc275e9495c6
David Vu <dvu4911@gmail.com>
2020-07-30 19:17:35 -0600

    Add files via upload

---
+++ Methods Galore/Methods Galore/methods-galore.js
@@ -0,0 +1,96 @@
+const listOfStrings = [
+    "Pack my box with five dozen liquor jugs.", 
+    "The quick brown fox jumps over the lazy dog.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+]
+
+// Set String onto HTML
+
+const setString = function (id, data) {
+    const pString =  document.getElementById(id);
+    pString.textContent = data
+}
+
+console.log(listOfStrings[0])
+
+setString("string-city", listOfStrings[0])
+setString("string-town", listOfStrings[1])
+setString("string-village", listOfStrings[2])
+
+// Button to get length of string
+
+document.getElementById("length-gen").onclick = function() {
+    const stringLength1 = listOfStrings[0].length
+    const stringLength2 = listOfStrings[1].length
+    const stringLength3 = listOfStrings[2].length
+    const htmlStringLength1 = document.getElementById('string-length-results-1');
+    htmlStringLength1.innerHTML = "The first string's length is " + stringLength1 + " characters long." + "<br>" + "The second string's length is " + stringLength2 + " characters long." + "<br>" + "The third string's length is a whopping " + stringLength3 + " characters long."    
+    
+}
+
+// Button to Split chars into items of an array
+
+document.getElementById("split-gen").onclick = function() {
+    const stringSplit1 = listOfStrings[0].split(' ')
+    const stringSplit2 = listOfStrings[1].split(' ')
+    const stringSplit3 = listOfStrings[2].split(' ', 10)
+    document.getElementById('string-split-1').textContent = stringSplit1
+    document.getElementById('string-split-2').textContent = stringSplit2
+    document.getElementById('string-split-3').innerHTML = stringSplit3 + "<br>" + "-This string has been limited by the first 10 words to conserve space."
+
+}
+
+// Button to Split characters, then get the length of those chars => Word Counter
+
+document.getElementById("split-length-gen").onclick = function() {
+    const stringSplit1 = listOfStrings[0].split(' ')
+    const stringSplit2 = listOfStrings[1].split(' ')
+    const stringSplit3 = listOfStrings[2].split(' ')
+    const stringSplitLength1 = stringSplit1.length
+    const stringSplitLength2 = stringSplit2.length
+    const stringSplitLength3 = stringSplit3.length
+    document.getElementById('string-split-length-1').textContent = "There are " + stringSplitLength1 + " words in the first string."
+    document.getElementById('string-split-length-2').textContent = "There are " + stringSplitLength2 + " words in the second string."
+    document.getElementById('string-split-length-3').textContent = "There are " + stringSplitLength3 + " words in the third string."
+    
+}
+
+// Button to Replace First Text(This) with Second Text(That) WIP**
+
+const userInputReplace = {
+    replacedWords: "" ,
+    
+}
+
+
+
+document.getElementById("replace-btn").onclick = function() {
+    // const userInputRegex = document.getElementById("replaced-string")
+    // userInputRegex = ""
+    // console.log(userInputRegex)
+    const userInputReplacingWord = document.getElementById("replacing-string").value
+    const userInputReplacedWord = document.getElementById("replaced-string").value
+    const find = new RegExp(document.getElementById("find").value, 'gi')
+
+    const replace = userInputReplacingWord.replace(find, userInputReplacedWord)
+    
+    console.log(replace)
+    // const replacedWords = listOfStrings.replace(userInputReplace.replacedWords, userInputReplacingWord)
+    // document.getElementById("strings-to-be-replaced").textContent = replacedWords
+
+}
+
+const exampleArray = [{
+    title: "The Full Caterpillar",
+    entry: "There was once a caterpillar like no other. And then it become even more so. A caterpillar who ate far too much and stayed that way. It was a strange Saturday afternoon. The sun was just hot enough where each step gave a tingle to the lowly caterpillar. But it was not enough to stopped the caterpillar from seeking its food, its destiny. Its destination was one that always seemingly evaded the caterpillar, but that never gave defeat onto the caterpillar. No. It only just motivated the caterpillar even more. It made the caterpillar even more hungry despite its fullness.",
+    date: "September 20, 2018"
+}, {
+    title: "False Memories",
+    entry: "I get fond of memories. Nostalgic even. Something about looking back just makes me look forward to so much in the future. At times, I would remember something and get so excited to talk about it with my wife. I tell her, expecting her remember the same memory as well, but she just looks at me with a raised eyebrow giving me a concerning stare that just pierces my soul and well-being. She rejects the notion that she was a part of that memory and exclaims that it was probably with somebody else. It makes me question the validity of my memories. Can I even trust those mememories? If I can't trust my memory, how can I even trust myself? Hell, I don't even trust me right now.",
+    date: "November 11, 2019"
+}, {
+    title: "The Routine",
+    entry: "Wake up at 6:00AM. Drink the full cup of water on the nightstand. Go to bathroom to brush teeth, swish mouthwash, wash face, and rinse. Then prep lunch and go to work with it. Eat lunch at work. Come back home. Grab hammer and swing at tire a bunch. Shower. Go to bed. ",
+    date: "June 16th, 2015"
+}]
+
