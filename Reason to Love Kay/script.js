e8e013f810a3e18bb000d1a5423143f84f3fa67e
David Vu <dvu4911@gmail.com>
2020-09-02 14:50:16 -0700

    finished recall btn

--- Reason to Love Kay/script.js
+++ Reason to Love Kay/script.js
@@ -3,9 +3,10 @@ const reasonsToLove = []
 
 const addBtn = document.querySelector("#add-btn") 
 const recallBtn = document.querySelector("#recall-btn")
+const generateBtn = document.querySelector("#generate-btn")
 
 addBtn.addEventListener("click", function toggleDisplay () {
-    let theDisplay = document.getElementsByClassName("form-display")
+    let theDisplay = document.getElementById("form-display-add")
     if (theDisplay.style.display === 'none') {
         theDisplay.style.display = "block";
     } else {
@@ -14,7 +15,24 @@ addBtn.addEventListener("click", function toggleDisplay () {
 })
 
 recallBtn.addEventListener("click", function toggleDisplay () {
-    let theDisplay = document.getElementsByClassName("form-display")
+    let theDisplay = document.getElementById("form-display-recall")
+    if (theDisplay.style.display === 'none') {
+        theDisplay.style.display = "block";
+    } else {
+        theDisplay.style.display = "none";
+    }
+})
+
+generateBtn.addEventListener("click", function toggleDisplay () {
+    let theDisplay = document.getElementById("form-display-generate")
+    const genRandom = Math.floor(Math.random() * reasonsToLove.length)
+    console.log(headerText)
+    headerText.innerHTML = "Hello World";
+
+
+
+
+
     if (theDisplay.style.display === 'none') {
         theDisplay.style.display = "block";
     } else {
