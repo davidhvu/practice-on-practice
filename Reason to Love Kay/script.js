000098ba03f8d3afa0841bd606347c52da7ce015
David Vu <dvu4911@gmail.com>
2020-09-01 22:29:11 -0700

    added more btn, adjust JS

--- Reason to Love Kay/script.js
+++ Reason to Love Kay/script.js
@@ -1,14 +1,20 @@
-// const addBtn = document.querySelector("#add-btn")
-// const theDisplay = document.querySelector("#add-display")
+const reasonsToLove = []
 
-// addBtn.addEventListener("click",function () {
-//     theDisplay.classList.toggle("hide")
-// })
 
 const addBtn = document.querySelector("#add-btn") 
+const recallBtn = document.querySelector("#recall-btn")
 
 addBtn.addEventListener("click", function toggleDisplay () {
-    const theDisplay = document.getElementById("add-display")
+    let theDisplay = document.getElementsByClassName("form-display")
+    if (theDisplay.style.display === 'none') {
+        theDisplay.style.display = "block";
+    } else {
+        theDisplay.style.display = "none";
+    }
+})
+
+recallBtn.addEventListener("click", function toggleDisplay () {
+    let theDisplay = document.getElementsByClassName("form-display")
     if (theDisplay.style.display === 'none') {
         theDisplay.style.display = "block";
     } else {
