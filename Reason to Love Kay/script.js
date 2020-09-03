7c9e3ced1565eae851437e0857cd9eda1be4fc55
David Vu <dvu4911@gmail.com>
2020-09-03 09:33:18 -0700

    added generate sect, fixed gen-random btn asap

--- Reason to Love Kay/script.js
+++ Reason to Love Kay/script.js
@@ -1,5 +1,9 @@
-const reasonsToLove = []
-
+const reasonsToLove = [
+    "She's the girl of my dreams.",
+    "I love that she makes me laugh just as much as I make her laugh",
+    "I love her family(so far) and I am so honored to be welcomed into this family. I hope I can prove myself! She's stupidly silly, I hope that part of her never changes",
+    "Kay gushed. Gushed. And gushed some more. That kiss meant a lot to her. For me, seeing her gush, that meant the world to me. Thank you, God"
+]
 
 const addBtn = document.querySelector("#add-btn") 
 const recallBtn = document.querySelector("#recall-btn")
@@ -24,18 +28,28 @@ recallBtn.addEventListener("click", function toggleDisplay () {
 })
 
 generateBtn.addEventListener("click", function toggleDisplay () {
+    
     let theDisplay = document.getElementById("form-display-generate")
-    const genRandom = Math.floor(Math.random() * reasonsToLove.length)
-    console.log(headerText)
-    headerText.innerHTML = "Hello World";
-
-
-
-
-
     if (theDisplay.style.display === 'none') {
         theDisplay.style.display = "block";
     } else {
         theDisplay.style.display = "none";
     }
 })
+
+document.querySelector("#gen-reason").onclick = function generateReason () {
+    const genRandom = Math.floor(Math.random() * reasonsToLove.length)
+    const genReason = (genRandom, reasonsToLove[genRandom])
+    const strArr = JSON.stringify(genReason, null, 4)
+    const getGenRand = function (arr) {
+        const reasonNumberDOM = document.createElement("h4")
+        const reasonDOM = document.createElement("p")
+        reasonDOM.innerHTML =  `${strArr}`
+        return reasonDOM 
+        console.log(reasonDOM)
+    }
+
+    document.querySelector("#random-reason-box").innerHTML = ""
+    document.querySelector("#random-reason-box").appendChild(getGenRand(reasonsToLove))
+
+}
