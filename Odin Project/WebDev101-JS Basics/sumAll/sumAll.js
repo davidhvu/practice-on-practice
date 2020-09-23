b81028246be2d370c429aea51f3bd1548ffa4739
David Vu <dvu4911@gmail.com>
2020-09-22 23:07:12 -0600

    Add files via upload

--- Odin Project/WebDev101-JS Basics/sumAll/sumAll.js
+++ Odin Project/WebDev101-JS Basics/sumAll/sumAll.js
@@ -1,23 +1,17 @@
-const sumAll = function(num1, num2) {
-    let arr = []
-    let reducer = (a, b) => a + b;
-    if (num1 < num2) {
-        for (let i = num1; i <= num2; i++) {
-        arr.push(i)
+    const sumAll = function(min, max) {
+        if (min < 0 || max < 0) return "ERROR";
+        if (typeof min !== "number" || typeof max !== "number") return "ERROR";
+        if (min > max) {
+          const temp = min;
+          min = max;
+          max = temp;
         }
-    } else if (num1 > num2) {
-        for (let i = num2; i <= num1; i++) {
-            arr.push(i)
-            }
-    } else if (num1 < 0 || num2 < 0) {
-        return 'ERROR'
-    } else if (!isNaN(num1) || !isNaN(num2)) {
-        return 'ERROR' 
-}    else   {
-        return 'ERROR'
-    }
-    let totalSum = arr.reduce(reducer, 0)
-        return totalSum 
-}
+        let sum = 0;
+        for (let i = min; i < max + 1; i++) {
+          sum += i;
+        }
+        return sum;
+      };
+
 
 module.exports = sumAll
